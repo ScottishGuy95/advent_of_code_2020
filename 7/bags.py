8927319cb787ba88bc625480438d08789e0234bb
ScottishGuy95 <32342101+ScottishGuy95@users.noreply.github.com>
2020-12-07 14:57:15 +0000

    Adding the blank file before I state Day 7

---
+++ 7/bags.py
@@ -0,0 +1,22 @@
+#! python3
+# bags.py - # TODO: Add explanation
+# Advent Of Code 2020 - Day 7
+
+filename = "input.txt"
+
+
+def part1():
+    return 1
+
+
+def part2():
+    return 2
+
+
+# Loop through the data, ...
+with open(filename) as f:
+    questions = f.read().split("\n\n")
+
+
+# print("Part 1: " + part1())
+# print("Part 2: " + part2())
